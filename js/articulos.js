e230f4f773b5258b7768591bc8a5355feb823b37
ERroberson <enrique.roberson@gmail.com>
2025-09-03 17:08:35 -0600

    Carga de articulos y SKU
    Se añadieron, 32 nuevos articulos, BloodMoonDiana, JaneDoe, y se añadieron los SKUs

--- js/articulos.js
+++ js/articulos.js
@@ -1,6 +1,7 @@
 // window.ARTICULOS quedará disponible globalmente
 window.ARTICULOS = [
     {
+        sku: 101000,
         categoria: "Anime",
         subcategoria: "Demon Slayer (Kimetsu no Yaiba)",
         nombre: "Tanjiro Kamado",
@@ -11,18 +12,20 @@ window.ARTICULOS = [
         dimensiones_cm: { ancho: 10, largo: 8, alto: 26 }
     },
     {
-        categoria: "Anime",
-        subcategoria: "Demon Slayer (Kimetsu no Yaiba)",
-        nombre: "Nezuko Kamado",
-        imgs: ["../img/Anime/DemonSlayer/Nezuko/Nezuko1.webp", "../img/Anime/DemonSlayer/Nezuko/Nezuko2.webp"],
+        sku: 301000,
+        categoria: "Videojuegos",
+        subcategoria: "League of Legends (LoL)",
+        nombre: "BloodMoon Diana",
+        imgs: ["../img/VideoGames/LeagueOfLegends/BloodMoonDiana/BloodMoonDiana.png", "../img/VideoGames/LeagueOfLegends/BloodMoonDiana/BloodMoonDiana2.webp", "../img/VideoGames/LeagueOfLegends/BloodMoonDiana/BloodMoonDiana3.webp", "../img/VideoGames/LeagueOfLegends/BloodMoonDiana/BloodMoonDiana4.webp"],
         descripcion: "Nezuko Kamado es la hermana menor de Tanjiro...",
         status: "Disponible",
         precio: "$1,900.00",
         dimensiones_cm: { ancho: 38, largo: 36, alto: 80 }
     }, {
+        sku: 101001,
         categoria: "Anime",
         subcategoria: "Demon Slayer (Kimetsu no Yaiba)",
-        nombre: "Nezuko Kamado Adulta",
+        nombre: "Nezuko Kamado Demonio",
         imgs: ["../img/Anime/DemonSlayer/Nezuko/Nezuko1.webp", "../img/Anime/DemonSlayer/Nezuko/Nezuko2.webp"],
         descripcion: "Nezuko Kamado es la hermana menor de Tanjiro...",
         status: "Disponible",
@@ -30,7 +33,7 @@ window.ARTICULOS = [
         dimensiones_cm: { ancho: 38, largo: 36, alto: 80 }
     },
     {
-        sku: 15646,
+        sku: 102000,
         categoria: "Anime",
         subcategoria: "Attack on Titan",
         nombre: "Eren Yeager",
@@ -41,6 +44,7 @@ window.ARTICULOS = [
         dimensiones_cm: { ancho: 9, largo: 6, alto: 15 }
     },
     {
+        sku: 401000,
         categoria: "Dungeons & Dragons (D&D)",
         subcategoria: "Barbaro",
         nombre: "Gu-Klu",
@@ -51,6 +55,7 @@ window.ARTICULOS = [
         dimensiones_cm: null
     },
     {
+        sku: 402000,
         categoria: "Dungeons & Dragons (D&D)",
         subcategoria: "Bardo",
         nombre: "Ciryn",
@@ -61,30 +66,34 @@ window.ARTICULOS = [
         dimensiones_cm: null
     },
     {
-        categoria: "El señor de los anillos",
-        subcategoria: "Istar",
+        sku: 403000,
+        categoria: "Dungeons & Dragons (D&D)",
+        subcategoria: "The Lord of the Rings",
         nombre: "Gandalf el Gris",
         imgs: ["../img/ElSeñorDeLosAnillos/Istar/Gandalf/Gandalf1.jpg", "../img/ElSeñorDeLosAnillos/Istar/Gandalf/Gandalf2.jpg"],
         descripcion: "Artistas y hechiceros que utilizan la música y la persuasión.",
         status: "Bajo pedido",
         precio: "",
         dimensiones_cm: null
+
     }, {
-        categoria: "Anime",
-        subcategoria: "Zoneless Zone Zero",
-        nombre: "Juanita",
-        imgs: ["../img/ElSeñorDeLosAnillos/Istar/Gandalf/Gandalf1.jpg", "../img/ElSeñorDeLosAnillos/Istar/Gandalf/Gandalf2.jpg"],
-        descripcion: "Artistas y hechiceros que utilizan la música y la persuasión.",
+        sku: 302000,
+        categoria: "Videojuegos",
+        subcategoria: "Zenless Zone Zero",
+        nombre: "Jane Doe",
+        imgs: ["../img/VideoGames/ZenlessZoneZero/JaneDoe/JaneDoe1.jpg", "../img/VideoGames/ZenlessZoneZero/JaneDoe/JaneDoe2.jpg", "../img/VideoGames/ZenlessZoneZero/JaneDoe/JaneDoe3.jpg", "../img/VideoGames/ZenlessZoneZero/JaneDoe/JaneDoe4.jpg"],
+        descripcion: "Es una agente jugable en Zenless Zone Zero y una especialista en comportamiento criminal que trabaja como consultora para el Equipo de Respuesta Especial de Investigación Criminal (Jane Doe).",
         status: "Bajo pedido",
         precio: "",
         dimensiones_cm: null
     },
     {
+        sku: 303000,
         categoria: "Videojuegos",
         subcategoria: "Warhammer 40K",
-        nombre: "Titus",
+        nombre: "Demetrian Titus",
         imgs: ["../img/VideoGames/Warhammer40K/Titus/Titus1.jpeg", "../img/VideoGames/Warhammer40K/Titus/Titus2.jpeg", "../img/VideoGames/Warhammer40K/Titus/Titus3.jpeg"],
-        descripcion: "Comandante Titus del primer regimiento de Space Marines.",
+        descripcion: "Hubo un tiempo en el que parecía que Demetrian Titus, capitán de la 2.ª Compañía, eclipsaría a todos, antes de que un giro de los acontecimientos lo llevara al exilio y se le presumiera muerto durante décadas.",
         status: "Bajo pedido",
         precio: "",
         dimensiones_cm: null
